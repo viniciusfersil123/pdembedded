865a8251a8430282b04fb1ee0203e695fd639e70
viniciusfersil123 <viniciusfersil@gmail.com>
2026-06-09 16:01:15 +0200

    changes pins and fix cmakeslist

--- main/pdembedded.cpp
+++ main/pdembedded.cpp
@@ -1,8 +1,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "drivers/uda1334a/uda1334a.h"
-#include "c/HeavyContextInterface.hpp"
-#include "c/Heavy_heavy.h"
+#include "build/c/HeavyContextInterface.hpp"
+#include "build/c/Heavy_heavy.h"
 
 static UDA1334A dac;
 static constexpr int SR = 48000;
@@ -13,7 +13,7 @@ static float* out_channels[2] = {out_ch0, out_ch1};
 
 extern "C" void app_main(void)
 {
-    dac.begin(SR, GPIO_NUM_27, GPIO_NUM_26, GPIO_NUM_25);
+    dac.begin(SR, GPIO_NUM_21, GPIO_NUM_23, GPIO_NUM_22);
     HeavyContextInterface *hv_ctx = hv_heavy_new(SR);
 
     while (true)
